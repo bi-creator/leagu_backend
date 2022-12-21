932c6639744f6c4cb10a5e5eb591f5b60e67b8c4
bi-creator <manjithreddy096@gmail.com>
2022-12-21 18:59:19 +0530

    updated queries and added new routes

--- main.py
+++ main.py
@@ -26,7 +26,10 @@ where st.slot_id not in (select slot_id from public.booking_table where ground_i
 
 @app.get('/getBookings')
 def getBookings():
-    data=executeQuery('select * FROM public.booking_table where is_cancled=false ')
+    data=executeQuery("""select bt.name,bt.email,bt.contact,pg.ground_name,sl.slot_time FROM public.booking_table bt
+join public.play_grounds pg ON pg.ground_id = bt.ground_id
+join public.slots sl ON sl.slot_id = bt.slot_id
+where is_cancled=false and TO_DATE(booking_id,'YYYY-MM-DD HH24:MI:SS')=CAST( NOW() AS Date)""")
     return(data)
 
 @app.post('/addBooking')
@@ -36,9 +39,18 @@ def addBooking(name, email,contact,ground_id,slot_id):
     data=executeQuery("INSERT INTO booking_table (booking_id, name, email,contact,ground_id,slot_id) VALUES (%s,%s,%s,%s,%s,%s)",datatoinsert)
     return data
 
-@app.post('/cancleBooking')
+@app.put('/cancleBooking')
 def cancleBooking(booking_id):
     data=executeQuery("UPDATE booking_table SET is_cancled = %s WHERE booking_id = %s",(True,booking_id))
     return data
 
-    
+@app.delete('/deletebookings')
+def deletebookings():
+    data=executeQuery("DELETE FROM public.booking_table WHERE TO_DATE(booking_id,'YYYY-MM-DD HH24:MI:SS')<(CAST( NOW() AS Date)-2);")
+    return data  
+
+
+@app.put('/updatedcouty')
+def updatedcouty(court_maintainence:bool,court_id):
+    data=executeQuery("UPDATE public.play_grounds SET under_maintainence = %s WHERE ground_id = %s",(court_maintainence,court_id))
+    return data
--- query.py
+++ query.py
@@ -5,19 +5,33 @@ def executeQuery(query,datatoinsert=[]):
         )
     cursor = conn.cursor()
     if(datatoinsert):
-        print(query,datatoinsert)
-        cursor.execute(query,datatoinsert)
-        conn.commit()
-        count = cursor.rowcount
-        return "Updated successfully into mobile table"
+        try:
+            # print(query,datatoinsert)
+            cursor.execute(query,datatoinsert)
+            conn.commit()
+            # count = cursor.rowcount
+            return "Updated successfully "
+        except:
+            return("Something Went Wrong")
+    elif('DELETE' in query):
+        try:
+            cursor.execute(query)
+            conn.commit()
+            return("Data Deleted")
+        except:
+            return("Something Went Wrong")
+
     else:
-        cursor.execute(query)
-        data=[]
-        column_names = [desc[0] for desc in cursor.description]
-        for row in cursor:
-            a={}
-            for col in  column_names:
-                a[col]=row[column_names.index(col)]
-            data.append(a)
-        conn.close()
-        return(data)
+        try:
+            cursor.execute(query)
+            data=[]
+            column_names = [desc[0] for desc in cursor.description]
+            for row in cursor:
+                a={}
+                for col in  column_names:
+                    a[col]=row[column_names.index(col)]
+                data.append(a)
+            conn.close()
+            return(data)
+        except:
+            return("Something Went Wrong")
