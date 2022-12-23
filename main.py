625b74c8dd4436e2fea157e98608e325e7ffc65d
bi-creator <manjithreddy096@gmail.com>
2022-12-24 02:19:43 +0530

    updated time stamp

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from query import executeQuery
 from datetime import datetime,timedelta
+import pytz
+IST = pytz.timezone('Asia/Kolkata')
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -14,13 +16,13 @@ app.add_middleware(
 
 @app.post('/addCourts')
 def addCourts(undermaintainence:bool,court_name):
-    court_id=str(datetime.now())
+    court_id=str(datetime.now(IST))
     data=executeQuery(f"INSERT INTO play_grounds ('ground_id','ground_name','under_maintainence') VALUES ('{court_id}','{court_name}','{undermaintainence}')")
     return data
 
 @app.post('/addSlots')
 def addSlots(slot_time):
-    slot_id=str(datetime.now())
+    slot_id=str(datetime.now(IST))
     data=executeQuery(f"INSERT INTO slots ('slot_id','slot_time') VALUES ('{slot_id}','{slot_time}')")
     return data
 
@@ -33,7 +35,7 @@ def getCourts():
 @app.get('/getAvailableSlots')
 def getSlots(court_id):
     print(court_id)
-    data=executeQuery(f"select st.slot_id,st.slot_time from slots st where (st.slot_id not in (select slot_id from booking_table bt where bt.ground_id='{court_id}' and substr(bt.booking_id, 1, 10)= '{str(datetime.now())[0:10]}' and bt.is_cancled=false)) or (st.slot_id in (select slot_id from booking_table bt where bt.ground_id='{court_id}' and substr(bt.booking_id, 1, 10)='{str(datetime.now())[0:10]}' and bt.is_cancled=true))")
+    data=executeQuery(f"select st.slot_id,st.slot_time from slots st where (st.slot_id not in (select slot_id from booking_table bt where bt.ground_id='{court_id}' and substr(bt.booking_id, 1, 10)= '{str(datetime.now(IST))[0:10]}' and bt.is_cancled=false)) or (st.slot_id in (select slot_id from booking_table bt where bt.ground_id='{court_id}' and substr(bt.booking_id, 1, 10)='{str(datetime.now(IST))[0:10]}' and bt.is_cancled=true))")
     return data
 
 @app.get('/getBookings')
@@ -41,12 +43,12 @@ def getBookings():
     data=executeQuery(f"""select bt.booking_id,bt.name,bt.email,bt.contact,pg.ground_name,sl.slot_time FROM booking_table bt
 join play_grounds pg ON pg.ground_id = bt.ground_id
 join slots sl ON sl.slot_id = bt.slot_id
-where is_cancled=false and substr(bt.booking_id, 1, 10)= '{str(datetime.now())[0:10]}'""")
+where is_cancled=false and substr(bt.booking_id, 1, 10)= '{str(datetime.now(IST))[0:10]}'""")
     return(data)
 
 @app.post('/addBooking')
 def addBooking(name, email,contact,ground_id,slot_id):
-    booking_id=str(datetime.now())
+    booking_id=str(datetime.now(IST))
     data=executeQuery(f"INSERT INTO booking_table (booking_id, name, email,contact,ground_id,slot_id) VALUES ('{booking_id}','{name}','{email}','{contact}','{ground_id}','{slot_id}')")
     return data
 
@@ -57,7 +59,7 @@ def cancleBooking(booking_id):
 
 @app.delete('/deletebookings')
 def deletebookings():
-    data=executeQuery(f"DELETE FROM booking_table WHERE booking_id<'{str(datetime.now()-timedelta(days=2))}';")
+    data=executeQuery(f"DELETE FROM booking_table WHERE booking_id<'{str(datetime.now(IST)-timedelta(days=2))}';")
     return data  
 
 
